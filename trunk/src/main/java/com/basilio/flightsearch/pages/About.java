a0939544c68d10ada3778a0b0195df9d9729ca69
bgerman@nearsoft.com <bgerman@nearsoft.com@0d868d50-6bdd-ee5c-2e01-d34e3b9e2719>
2012-06-23 04:34:53 +0000

    Updated layout, added NotAuthenticated tag, added empty About page and class.

---
+++ trunk/src/main/java/com/basilio/flightsearch/pages/About.java
@@ -0,0 +1,14 @@
+package com.basilio.flightsearch.pages;
+
+import com.basilio.flightsearch.annotations.GuestAccess;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cetobasilius
+ * Date: 6/22/12
+ * Time: 9:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@GuestAccess
+public class About {
+}
