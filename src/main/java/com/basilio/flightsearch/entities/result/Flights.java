adfc5a1b0e0882de3f50f3d3604b308ff59013ac
bgerman <bgerman@nearsoft.com>
2012-07-13 21:45:24 +0000

    Worked on result page view.

--- src/main/java/com/basilio/flightsearch/entities/result/Flights.java
+++ src/main/java/com/basilio/flightsearch/entities/result/Flights.java
@@ -1,6 +1,7 @@
 
 package com.basilio.flightsearch.entities.result;
 
+import java.util.ArrayList;
 import java.util.List;
 /**
  * Created with IntelliJ IDEA.
@@ -53,4 +54,28 @@ public class Flights{
 	public void setPriceInfo(PriceInfo priceInfo){
 		this.priceInfo = priceInfo;
 	}
+
+
+    public String[] toStringArray() {
+        List<String> stringList = new ArrayList<String>();
+        List<OutboundRoutes> outboundRoutes1 = this.getOutboundRoutes();
+        int numOutboundRoutes = outboundRoutes1.size();
+        stringList.add("This flight has "+numOutboundRoutes+" outbound routes.");
+
+        for(int a = 0; a<numOutboundRoutes;a++){
+            List<Segments> segments = outboundRoutes1.get(a).getSegments();
+            int numSegments = segments.size();
+            stringList.add("  * Outbound route " + a + " has " + numSegments + " segments:");
+            for(int b = 0;b<numSegments;b++){
+                stringList.add("    * Segment " + b + " flight number is: " + segments.get(b).getFlightNumber());
+            }
+        }
+
+        String[] stringArray = new String[stringList.size()];
+        stringList.toArray(stringArray);
+
+        return stringArray;
+    }
+
+
 }
