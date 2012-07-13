89bb47abe13915ec84cc491cfb121670c282b5e9
bgerman@nearsoft.com <bgerman@nearsoft.com@0d868d50-6bdd-ee5c-2e01-d34e3b9e2719>
2012-07-13 21:45:24 +0000

    Worked on result page view.

--- trunk/src/main/java/com/basilio/flightsearch/pages/ResultsPage.java
+++ trunk/src/main/java/com/basilio/flightsearch/pages/ResultsPage.java
@@ -3,9 +3,11 @@ package com.basilio.flightsearch.pages;
 import com.basilio.flightsearch.annotations.GuestAccess;
 import com.basilio.flightsearch.entities.ResultCreator;
 import com.basilio.flightsearch.entities.Search;
+import com.basilio.flightsearch.entities.result.Flights;
 import com.basilio.flightsearch.entities.result.Result;
 import org.apache.tapestry5.PersistenceConstants;
 import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.Zone;
@@ -27,11 +29,11 @@ public class ResultsPage {
 
 
     @Property
-    @Persist(PersistenceConstants.FLASH)
+    @Persist(PersistenceConstants.SESSION)
     private String origin;
 
     @Property
-    @Persist(PersistenceConstants.FLASH)
+    @Persist(PersistenceConstants.SESSION)
     private String destination;
 
     @Property
@@ -40,15 +42,11 @@ public class ResultsPage {
     @Property
     private double lngin = 0;
 
-    @Persist(PersistenceConstants.FLASH)
+    @Persist(PersistenceConstants.SESSION)
     @Property
     private List<Double> coordinatesParameter;
 
-    @InjectComponent
-    private Zone zone;
-
-
-    @Persist(PersistenceConstants.FLASH)
+    @Persist(PersistenceConstants.SESSION)
     private Result result;
 
     public void setup(Search search,Result result)
@@ -63,7 +61,7 @@ public class ResultsPage {
         System.out.println(coordinatesParameter);
     }
 
-    Object onActionFromChangeMapTest()
+    void onActionFromChangeMapTest()
     {
         List<Double> setupList = new ArrayList<Double>();
         setupList.add(-90.0+(Math.random()*180));setupList.add(20.0+(Math.random()*20));
@@ -72,7 +70,6 @@ public class ResultsPage {
         setupList.add(-90.0+(Math.random()*180));setupList.add(-179.0+(Math.random()*20));
 
         this.setupGMap(setupList);
-        return zone;
     }
 
     public String getNumFlights(){
@@ -84,23 +81,31 @@ public class ResultsPage {
     }
 
     @Property
-    private String _resultString;
+    private Flights flight;
 
-    public String[] getResults()
+    @Property
+    private String flightString;
+
+    public String[] getFlightStrings(){
+        return flight.toStringArray();
+    }
+
+    public Flights[] getFlights()
     {
         int numFlights = 0;
-        String[] resultArray = null;
+        Flights[] resultArray = null;
         if(result != null){
 
             numFlights = result.getFlights().size();
-            resultArray = new String[numFlights];
+            resultArray = new Flights[numFlights];
 
             for(int a = 0;a<numFlights;a++){
-                resultArray[a] = "This flight has "+result.getFlights().get(a).getOutboundRoutes().size()+" outbound routes.";
+                //resultArray[a] = "This flight has "+result.getFlights().get(a).getOutboundRoutes().size()+" outbound routes.";
+                resultArray[a] = result.getFlights().get(a);
             }
         } else {
-            resultArray = new String[1];
-            resultArray[0] = "There are no results to display.";
+            resultArray = new Flights[1];
+            resultArray[0] = new Flights();
         }
         return resultArray;
     }
