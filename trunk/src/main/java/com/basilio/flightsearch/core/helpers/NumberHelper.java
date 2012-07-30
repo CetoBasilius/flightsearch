77c45a43b0bfcfaebe5f32a62f96919947f025d0
bgerman@nearsoft.com <bgerman@nearsoft.com@0d868d50-6bdd-ee5c-2e01-d34e3b9e2719>
2012-07-30 15:21:23 +0000

    Rearranged results view

---
+++ trunk/src/main/java/com/basilio/flightsearch/core/helpers/NumberHelper.java
@@ -0,0 +1,23 @@
+package com.basilio.flightsearch.core.helpers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cetobasilius
+ * Date: 7/30/12
+ * Time: 7:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NumberHelper {
+    public static String ordinal(int i) {
+        String[] sufixes = new String[] { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+        switch (i % 100) {
+            case 11:
+            case 12:
+            case 13:
+                return i + "th";
+            default:
+                return i + sufixes[i % 10];
+
+        }
+    }
+}
