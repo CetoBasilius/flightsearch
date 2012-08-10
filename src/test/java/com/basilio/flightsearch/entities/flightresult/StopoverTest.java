1b1b36f2625f51e0d898a35c69dc6b9dbf3f61c7
bgerman <bgerman@nearsoft.com>
2012-08-10 22:37:37 +0000

    Added missing unit tests

---
+++ src/test/java/com/basilio/flightsearch/entities/flightresult/StopoverTest.java
@@ -0,0 +1,30 @@
+package com.basilio.flightsearch.entities.flightresult;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bgerman
+ * Date: 8/10/12
+ * Time: 9:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StopoverTest {
+
+    @Test
+    public void testStopovers(){
+        Stopover stopover = new Stopover();
+        String code = "code";
+        stopover.setAirportCode(code);
+        String description = "description";
+        stopover.setAirportDescription(description);
+        String duration = "duration";
+        stopover.setDuration(duration);
+
+        assertEquals(code,stopover.getAirportCode());
+        assertEquals(description,stopover.getAirportDescription());
+        assertEquals(duration,stopover.getDuration());
+    }
+}
---
+++ src/test/java/com/basilio/flightsearch/mixins/ConfirmTest.java
@@ -0,0 +1,44 @@
+package com.basilio.flightsearch.mixins;
+
+import org.apache.tapestry5.ClientElement;
+import org.apache.tapestry5.dom.Element;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+import org.junit.Test;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bgerman
+ * Date: 8/10/12
+ * Time: 8:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConfirmTest {
+    @Test
+    public void testAfterRender(){
+        Confirm confirm = new Confirm();
+        String message = "This is a message";
+        ClientElement element = createNiceMock(ClientElement.class);
+        JavaScriptSupport javaScriptSupport = createNiceMock(JavaScriptSupport.class);
+
+        expect(element.getClientId()).andReturn("id").anyTimes();
+        javaScriptSupport.addScript("new Confirm('%s', '%s');", "id", message);
+        expectLastCall();
+
+
+        confirm.setMessage(message);
+        confirm.setJavaScriptSupport(javaScriptSupport);
+        confirm.setElement(element);
+        assertEquals(message,confirm.getMessage());
+        assertEquals(javaScriptSupport,confirm.getJavaScriptSupport());
+
+        replay(element,javaScriptSupport);
+
+        confirm.afterRender();
+
+        verify(element,javaScriptSupport);
+
+    }
+}
