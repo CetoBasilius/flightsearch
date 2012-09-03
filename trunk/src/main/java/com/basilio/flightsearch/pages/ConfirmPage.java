63dd150555eb1b7fb2fc7ef28383b7c8f9ad3038
bgerman@nearsoft.com <bgerman@nearsoft.com@0d868d50-6bdd-ee5c-2e01-d34e3b9e2719>
2012-09-03 17:04:49 +0000

    Updated Confirmpage

--- trunk/src/main/java/com/basilio/flightsearch/pages/ConfirmPage.java
+++ trunk/src/main/java/com/basilio/flightsearch/pages/ConfirmPage.java
@@ -1,14 +1,17 @@
 package com.basilio.flightsearch.pages;
 
 import com.basilio.flightsearch.annotations.GuestAccess;
+import com.basilio.flightsearch.components.Window;
+import com.basilio.flightsearch.core.helpers.NumberHelper;
 import com.basilio.flightsearch.entities.PaymentOption;
-import com.basilio.flightsearch.entities.flightresult.Flight;
-import com.basilio.flightsearch.entities.flightresult.FlightSearch;
+import com.basilio.flightsearch.entities.flightresult.*;
 import com.basilio.flightsearch.entities.Passenger;
-import com.basilio.flightsearch.entities.flightresult.Payment;
+import org.apache.commons.lang.WordUtils;
 import org.apache.tapestry5.ValueEncoder;
 import org.apache.tapestry5.annotations.*;
 import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -101,8 +104,62 @@ public class ConfirmPage {
     private FlightSearch flightSearch;
     @Persist
     private Flight flight;
+
+    @Property
+    @Inject
+    private Request request;
+    @Property
+    private int outSegmentWindowIndex;
+    @Property
+    private int inSegmentWindowIndex;
+
+    @Component(parameters = {"style=greylighting",
+            "show=false",
+            "modal=true",
+            "width=500",
+            "title=literal:Outbound flight details"})
+    private Window outboundWindow;
+
+    @Component(parameters = {"style=greylighting",
+            "show=false",
+            "modal=true",
+            "width=500",
+            "title=literal:Inbound flight details"})
+    private Window inboundWindow;
+
+    @Property
+    private int outBoundIndex;
+    @Property
+    private int inBoundIndex;
+
+    public Route getOutboundRoute(){
+        outboundRoute = flight.getOutboundRoutes().get(0);
+        return outboundRoute;
+    }
+
+    public Route getInboundRoute(){
+        inboundRoute = flight.getInboundRoutes().get(0);
+        return inboundRoute;
+    }
+
     @Persist
-    private FlightSearch search;
+    private Route outboundRoute;
+    @Persist
+    private Route inboundRoute;
+
+
+    @Property
+    private String outSegmentInfo;
+    @Property
+    private String inSegmentInfo;
+    @Property
+    private int outRouteSegmentInfoIndex;
+    @Property
+    private int inRouteSegmentInfoIndex;
+    @Property
+    private Segment outSegment;
+    @Property
+    private Segment inSegment;
 
     public String getFinalMessage(){
         return builtFinalMessage;
@@ -189,13 +246,12 @@ public class ConfirmPage {
     }
 
     public void setup(FlightSearch search, Flight flight) {
-        this.search = search;
-        if(passengerList == null){
-            passengerList = new ArrayList<Passenger>();
-        }
-        if(paymentOptionList == null){
-            paymentOptionList = new ArrayList<PaymentOption>();
-        }
+        this.flightSearch = search;
+        this.flight = flight;
+
+        passengerList = new ArrayList<Passenger>();
+        paymentOptionList = new ArrayList<PaymentOption>();
+
         for(int index = 0; index < search.getNumberAdults();index++){
             passengerList.add(new Passenger(Passenger.TYPE_ADULT));
         }
@@ -206,7 +262,9 @@ public class ConfirmPage {
             passengerList.add(new Passenger(Passenger.TYPE_INFANT));
         }
 
-        this.flight = flight;
+        getOutboundRoute();
+        getInboundRoute();
+
         List<Payment> payments = flight.getPaymentInfo().getPayments();
         for(int index = 0; index < payments.size();index++){
             PaymentOption paymentOption = new PaymentOption();
@@ -218,4 +276,234 @@ public class ConfirmPage {
     public boolean getPageReady(){
         return passengerList != null;
     }
+
+    public String getFlightOutLabelDesc() {
+        return flight.getOutDescription();
+    }
+
+    public String getFlightInLabelDesc() {
+        return flight.getInDescription();
+    }
+
+    public String getOutRouteScheduleInfo() {
+        return outboundRoute.getScheduleDescription();
+    }
+
+    public String getInRouteScheduleInfo() {
+        return inboundRoute.getScheduleDescription();
+    }
+
+    public String getOutRouteSegmentNumber() {
+        return outboundRoute.getSegmentsNumber();
+    }
+
+    public String getInRouteSegmentNumber() {
+        return inboundRoute.getSegmentsNumber();
+    }
+
+    public String[] getOutRouteSegmentInfo() {
+        return outboundRoute.getSegmentsDescription();
+    }
+
+    public String[] getInRouteSegmentInfo() {
+        return inboundRoute.getSegmentsDescription();
+    }
+
+    public boolean getInContinueRenderingArrow() {
+        if (inRouteSegmentInfoIndex < inboundRoute.getSegments().size()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean getOutContinueRenderingArrow() {
+        if (outRouteSegmentInfoIndex < outboundRoute.getSegments().size()) {
+            return true;
+        }
+        return false;
+    }
+
+    public Segment[] getOutSegments() {
+        return outboundRoute.getSegments().toArray(new Segment[0]);
+    }
+
+    public Segment[] getInSegments() {
+        return inboundRoute.getSegments().toArray(new Segment[0]);
+    }
+
+    @Property
+    private final ValueEncoder<Segment> outSegmentsValueEncoder = new ValueEncoder<Segment>() {
+
+        public String toClient(Segment answer) {
+            int in = outboundRoute.getSegments().indexOf(answer);
+            return String.valueOf(in);
+        }
+
+        public Segment toValue(String str) {
+            return null;
+        }
+    };
+
+    @Property
+    private final ValueEncoder<Segment> inSegmentsValueEncoder = new ValueEncoder<Segment>() {
+        public String toClient(Segment answer) {
+            int in = inboundRoute.getSegments().indexOf(answer);
+            return String.valueOf(in);
+        }
+
+        public Segment toValue(String str) {
+            return null;
+        }
+    };
+
+    public String getOutSegmentWindowDescription() {
+        return segmentWindowDescription(outSegment, outSegmentWindowIndex);
+    }
+
+    public String getInSegmentWindowDescription() {
+        return segmentWindowDescription(inSegment, inSegmentWindowIndex);
+    }
+
+    public String segmentWindowDescription(Segment segment, int index) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(NumberHelper.ordinal(index + 1));
+        buffer.append(" segment - Flight ");
+        buffer.append(segment.getFlightNumber());
+        buffer.append(" - ");
+        buffer.append(WordUtils.capitalize(segment.getMarketingCabinTypeCode().toLowerCase()));
+        buffer.append(" - ");
+        buffer.append(segment.getOperatingCarrierDescription());
+        return buffer.toString();
+    }
+
+    public String getOutSegmentLeaveInfo() {
+        return outSegment.getDepartureDescription();
+    }
+
+    public String getInSegmentLeaveInfo() {
+        return inSegment.getDepartureDescription();
+    }
+
+    public String getOutSegmentLeaveHourInfo() {
+        return outSegment.getDepartureHourDescription();
+    }
+
+    public String getInSegmentLeaveHourInfo() {
+        return inSegment.getDepartureHourDescription();
+    }
+
+    public boolean getOutSegmentHasStops() {
+        return outSegment.getStopovers() != null && outSegment.getStopovers().size() > 0;
+    }
+
+    public boolean getInSegmentHasStops() {
+        return inSegment.getStopovers() != null && inSegment.getStopovers().size() > 0;
+    }
+
+    public String getOutSegmentStopInfo() {
+        return outSegment.getStopDescription();
+    }
+
+    public String getInSegmentStopInfo() {
+        return inSegment.getStopDescription();
+    }
+
+    public String getOutSegmentArriveInfo() {
+        return outSegment.getArrivalDescription();
+    }
+
+    public String getInSegmentArriveInfo() {
+        return inSegment.getArrivalDescription();
+    }
+
+    public String getOutSegmentArriveHourInfo() {
+        return outSegment.getArrivalHourDescription();
+    }
+
+    public String getInSegmentArriveHourInfo() {
+        return inSegment.getArrivalHourDescription();
+    }
+
+    public String getOutSegmentDurationInfo() {
+        return outSegment.getDurationDescription();
+    }
+
+    public String getInSegmentDurationInfo() {
+        return inSegment.getDurationDescription();
+    }
+
+    public boolean getIsThereNextOutSegment() {
+        return (outSegmentWindowIndex < outboundRoute.getSegments().size() - 1);
+    }
+
+    public boolean getIsThereNextInSegment() {
+        return (inSegmentWindowIndex < inboundRoute.getSegments().size() - 1);
+    }
+
+    public String getInSegmentWaitDesc() {
+        return String.format("There will be a wait of  %s", inboundRoute.getWaitDescription(inSegmentWindowIndex, inSegmentWindowIndex + 1));
+    }
+
+    public String getOutSegmentWaitDesc() {
+        return String.format("There will be a wait of  %s", outboundRoute.getWaitDescription(outSegmentWindowIndex, outSegmentWindowIndex + 1));
+    }
+
+    public String getOutRouteDurationInfo() {
+        return outboundRoute.getDurationDescription();
+    }
+
+    public String getInRouteDurationInfo() {
+        return inboundRoute.getDurationDescription();
+    }
+
+    public String getOutRouteSegmentInfoCommas() {
+        return routeSegmentInfoCommas(outboundRoute);
+    }
+
+    public String getInRouteSegmentInfoCommas() {
+        return routeSegmentInfoCommas(inboundRoute);
+    }
+
+    private String routeSegmentInfoCommas(Route route) {
+        StringBuffer buffer = new StringBuffer();
+
+        String[] segmentsDescription = route.getSegmentsDescription();
+        int length = segmentsDescription.length;
+        for (int index = 0; index < length; index++) {
+            buffer.append(segmentsDescription[index]);
+            if (index < length - 1) {
+                buffer.append(",");
+            }
+        }
+        return buffer.toString();
+    }
+
+    public boolean getIsFlightRound() {
+        return flightSearch.isRoundTrip();
+    }
+
+    @Property
+    private final ValueEncoder<Route> outboundRoutesValueEncoder = new ValueEncoder<Route>() {
+        public String toClient(Route answer) {
+            int in = flight.getOutboundRoutes().indexOf(answer);
+            return String.valueOf(in);
+        }
+
+        public Route toValue(String str) {
+            return null;
+        }
+    };
+
+    @Property
+    private final ValueEncoder<Route> inboundRoutesValueEncoder = new ValueEncoder<Route>() {
+
+        public String toClient(Route answer) {
+            int in = flight.getInboundRoutes().indexOf(answer);
+            return String.valueOf(in);
+        }
+
+        public Route toValue(String str) {
+            return null;
+        }
+    };
 }
