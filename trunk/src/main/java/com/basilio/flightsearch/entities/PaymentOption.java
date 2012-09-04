9d1862073a656bc9fd60ad7ea5b45684dc141f99
bgerman@nearsoft.com <bgerman@nearsoft.com@0d868d50-6bdd-ee5c-2e01-d34e3b9e2719>
2012-09-04 19:35:13 +0000

    Updated confirmpage

--- trunk/src/main/java/com/basilio/flightsearch/entities/PaymentOption.java
+++ trunk/src/main/java/com/basilio/flightsearch/entities/PaymentOption.java
@@ -1,5 +1,8 @@
 package com.basilio.flightsearch.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: bgerman
@@ -10,12 +13,61 @@ package com.basilio.flightsearch.entities;
 public class PaymentOption {
 
     private String description;
+    private List<String> cards;
+
+    public List<String> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<String> cards) {
+        this.cards = cards;
+    }
+
+    public int getNumberOfPayments() {
+        return numberOfPayments;
+    }
+
+    public void setNumberOfPayments(int numberOfPayments) {
+        this.numberOfPayments = numberOfPayments;
+    }
+
+    public int getFirstPayment() {
+        return firstPayment;
+    }
+
+    public void setFirstPayment(int firstPayment) {
+        this.firstPayment = firstPayment;
+    }
+
+    public int getOtherPayments() {
+        return otherPayments;
+    }
+
+    public void setOtherPayments(int otherPayments) {
+        this.otherPayments = otherPayments;
+    }
+
+    private int numberOfPayments;
+
+    private int firstPayment;
+    private int otherPayments;
+
+    public PaymentOption(){
+        cards = new ArrayList<String>();
+    }
 
     public String getDescription() {
-        return description;
+        String paymentString = "Payment";
+        if(numberOfPayments>1){paymentString+="s";}
+        return numberOfPayments+" "+paymentString;
+
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getCardsString() {
+        return cards.toString();
     }
 }
